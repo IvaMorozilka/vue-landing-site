c4ebe57d39a5f3b822c17c5f6d1975acdbb6dbe4
Morozilka <87670176+IvaMorozilka@users.noreply.github.com>
2024-04-02 20:32:07 +0300

    init  placeholder

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,21 @@ import TheWelcome from './components/TheWelcome.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
+    </div> -->
+
+    <div>
+      <h1>Здесь будет разрабатываться сайт</h1>
     </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
+    <h2>Идет настройка проекта... Скоро появится хеадер :)</h2>
+    <img src="https://resizer.mail.ru/p/8e2e46c4-7137-5f91-8a6a-7bf4e963c943/AQACB_rBp9_0xvGVY942sS8CqVucCdtu8YKpYJacXOxNAs-gEpeDt2EturYJt-hSXunjquPgOmmBEuTD5ohw9vxeLUw.jpg">
   </main>
 </template>
 
